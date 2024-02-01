4cca73633dd70ecbaaf4e8d48f0cbc7343481389
Krishna Saxena <rio17105@gmail.com>
2024-02-01 12:22:41 +0530

    Login form updated with default values

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@gmail.com");
+  const [password, setPassword] = useState("12345678");
   const { mutate: login, isLoading } = useLogin();
 
   function handleSubmit(e) {
