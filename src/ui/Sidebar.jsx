33db0d9577b877f2436ff89fe8e34d8cc5dacf28
Krishna Saxena <rio17105@gmail.com>
2024-02-01 10:53:14 +0530

    Uploader component added

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const SidebarStyled = styled.aside`
   background-color: var(--color-grey-0);
@@ -17,6 +18,7 @@ function Sidebar() {
     <SidebarStyled>
       <Logo />
       <MainNav />
+      <Uploader />
     </SidebarStyled>
   );
 }
